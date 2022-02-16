e9ede7c98f405bd23f9266d7f3806de27c8e042c
dvicanajdinovic <80271605+dvicanajdinovic@users.noreply.github.com>
2022-02-16 20:52:43 +0100

    Add files via upload

---
+++ Kod/Kod/main.c
@@ -0,0 +1,18 @@
+/*
+ * Kod.c
+ *
+ * Created: 16.2.2022. 20:12:02
+ * Author : Diana
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
