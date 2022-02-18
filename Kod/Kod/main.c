da0a4787a1cdbdb502e1ca27e09a12cbc9a95bc4
Patrik <patrik@exevio.com>
2022-02-18 23:38:50 +0100

    Add motion sensor and buzzer functionality, adjust password input reset conditions

--- Kod/Kod/main.c
+++ Kod/Kod/main.c
@@ -26,9 +26,10 @@
 #define PASS_SIZE	6
 #define LCD_WIDTH   16
 
-uint8_t reset_set_password = 0;
-uint8_t active = 0;
+uint8_t reset_password_input = 0;
+uint8_t armed = 0;
 uint8_t password_set = 0;
+uint8_t movement_detected = 0;
 uint16_t timer0_multip = 0;
 
 uint8_t coordinates[2] = {3, 4};
@@ -64,49 +65,60 @@ void get_char() {
 	coordinates[1] = 4;
 }
 
-void check_password(char temp[]) {
-	if (!strcmp(temp, password)) {
+void check_password() {
+	if (!strcmp(temporary_password, password)) {
+		armed = 0;
+		movement_detected = 0;
+		
 		lcd_clrscr();
 		lcd_puts("You got it!");
-		
-		SIGNAL_DDR = _BV(LED);
-		} else {
+	} else {
 		lcd_clrscr();
 		lcd_puts("Pass incorrect");
-		SIGNAL_DDR = 0x00;
+
 	}
 }
 
 void enter_password() {
+	//disable timer0
+	TIMSK &= ~_BV(OCIE0);
+	
 	lcd_clrscr();
 	lcd_puts("Enter password:");
 	
-	uint8_t i = 0, counter = 0;
+	uint8_t i = 0;
 	while (i < PASS_SIZE) {
+		//enable timer0
+		if(i == 1) {
+			TIMSK = _BV(OCIE0);
+		}
 		get_char();
 		char c = keypad[coordinates[0]][coordinates[1]];
 		if (c != 'w') {
+			timer0_multip = 0;
 			temporary_password[i] = c;
 			lcd_gotoxy((LCD_WIDTH - PASS_SIZE) / 2 + i, 1);
 			lcd_putc(c);
 			i++;
-			counter = 0;
 		}
 		_delay_ms(200);
-		counter++;
-		if (counter == 50) {
-			break;
+		
+		if(reset_password_input) {
+			i = 0;
+			reset_password_input = 0;
+			lcd_clrscr();
+			lcd_puts("Enter password:");
 		}
+		
 	}
+	//disable timer0
+	TIMSK &= ~_BV(OCIE0);
+	
 	// call delay function, so the last '*' can be shown too
 	_delay_ms(1000);
 	
-	if (i < PASS_SIZE) {
-		enter_password();
-	} else {
-		temporary_password[PASS_SIZE] = '\0';
-		check_password(temporary_password);
-	}	
+	temporary_password[PASS_SIZE] = '\0';
+	check_password();
 }
 
 //dodaj neku bool varijablu koja ce kontrolirati smije li se unijeti novi pass
@@ -121,21 +133,20 @@ void change_password() {
 }
 
 void set_password() {
+	
 	//disable timer0
 	TIMSK &= ~_BV(OCIE0);
-	reset_set_password = 0;
 	lcd_clrscr();
 	lcd_puts("Please set your");
 	lcd_gotoxy(3, 1);
 	lcd_puts("password!");
-	_delay_ms(4000);
+	_delay_ms(1000);
 	lcd_clrscr();
 	lcd_home();
 	lcd_puts("Password:");
 	
 	uint8_t i = 0;
 	while (i < PASS_SIZE) {
-	
 		//enable timer0
 		if(i == 1) {
 			TIMSK = _BV(OCIE0);
@@ -151,8 +162,12 @@ void set_password() {
 		}
 		_delay_ms(200);
 		
-		if(reset_set_password) {
-			set_password();
+		if(reset_password_input) {
+			i = 0;
+			reset_password_input = 0;
+			lcd_clrscr();
+			lcd_home();
+			lcd_puts("Password:");
 		}
 	
 	}
@@ -165,10 +180,16 @@ void set_password() {
 	
 	lcd_clrscr();
 	lcd_gotoxy(1, 0);
+	password[PASS_SIZE] = '\0';
 	lcd_puts("Password set!");
 	_delay_ms(1000);
+	lcd_clrscr();
+	lcd_puts("Armed");
+	_delay_ms(1000);
+	
 
 	password_set = 1;
+	armed = 1;
 }
 
 ISR(TIMER0_COMP_vect) {
@@ -176,13 +197,16 @@ ISR(TIMER0_COMP_vect) {
 	//if(timer0_multip >= 280) {
 	if(timer0_multip >= 50) {
 		timer0_multip = 0;
-		reset_set_password = 1;
+		reset_password_input = 1;
 	}
 }
 
 int main(void)
 {
 	DDRD = _BV(4);
+	PIR_DDR = 0x00;
+	SIGNAL_DDR = 0xff;
+	SIGNAL_PRT = 0xff;
 	
 	//Timer 0 CTC mode, 1024 prescaler
 	TCCR0 = _BV(WGM01) | _BV(CS00) | _BV(CS02);
@@ -194,21 +218,28 @@ int main(void)
 	OCR1B = 128;
 	
 	lcd_init(LCD_DISP_ON);
-		
-	//DDR and PORT setup for LEDS (or buzzer)
-	SIGNAL_DDR = 0x00;
-	SIGNAL_PRT = 0x00;
 	
 	set_password();
 	
 	while (1) {
-		if (SIGNAL_DDR & 0x01) {
+		get_char();
+		char c = keypad[coordinates[0]][coordinates[1]];
+		_delay_ms(200);
+		if (!armed) {
+			SIGNAL_PRT = 0xff;
 			lcd_clrscr();
 			lcd_puts("Unlocked!");
-		} else {
+		} else if(c == 'A') {
 			enter_password();
+		} else {
+			//movement detected
+			if(PIR_PIN & (1 << 0)) {
+				SIGNAL_PRT = 0xfe;
+				movement_detected = 1;
+			}
+			
+			
 		}
-		_delay_ms(2000);
 	}
 }
 
